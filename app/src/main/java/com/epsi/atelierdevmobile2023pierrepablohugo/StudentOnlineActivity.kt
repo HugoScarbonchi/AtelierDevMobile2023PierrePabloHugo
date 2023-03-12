e6afe0384e48cea6a88a1fddd7b54ba690f2d69c
Hugo Scarbonchi <hugo.scarbonchi@mink-agency.com>
2023-03-12 12:11:32 +0100

    💄 (Update): Style changes on homepage and student lists

--- app/src/main/java/com/epsi/atelierdevmobile2023pierrepablohugo/StudentOnlineActivity.kt
+++ app/src/main/java/com/epsi/atelierdevmobile2023pierrepablohugo/StudentOnlineActivity.kt
@@ -13,7 +13,7 @@ class StudentOnlineActivity : BaseActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_student_online)
         showBack()
-        setHeaderTitle(getString(R.string.title_students))
+        setHeaderTitle(getString(R.string.title_infos))
 
         val url="https://img.freepik.com/photos-gratuite/rendu-3d-fond-espace-planetes-abstraites-nebuleuse_1048-12994.jpg?w=740&t=st=1673970224~exp=1673970824~hmac=5db29203b7bcddbecb50f50b6bab3920b729055ce5b87aaccc3260362a801f19"
         val students= arrayListOf<Student>()
